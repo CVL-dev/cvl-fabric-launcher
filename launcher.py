1b568b03b8cdb8c708aedfef0cdee07f5cb5a941
James Wettenhall <james.wettenhall@monash.edu>
2012-08-14 02:35:55 +0000

    Added GNU public license version 2 to source code.

--- launcher.py
+++ launcher.py
@@ -1,3 +1,22 @@
+#  MASSIVE/CVL Launcher - easy secure login for the MASSIVE Desktop and the CVL
+#  Copyright (C) 2012  James Wettenhall, Monash University
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#  Enquires: James.Wettenhall@monash.edu or help@massive.org.au
+
 # launcher.py
 """
 A wxPython GUI to provide easy login to the MASSIVE Desktop.
