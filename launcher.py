76fed441744a2d7e1b822da691e37dfe74ba94c1
James Wettenhall <james.wettenhall@monash.edu>
2012-08-28 05:11:53 +0000

    New version alert dialog now displays list of changes.

--- launcher.py
+++ launcher.py
@@ -107,27 +107,38 @@ class MyHtmlParser(HTMLParser.HTMLParser):
     HTMLParser.HTMLParser.__init__(self)
     self.recording = 0
     self.data = []
+    self.recordingLatestVersionNumber = 0
+    self.latestVersionNumber = "0.0.0"
+    self.htmlComments = ""
 
   def handle_starttag(self, tag, attributes):
     if tag != 'span':
       return
-    if self.recording:
-      self.recording += 1
-      return
+    if tag == "span":
+        if self.recordingLatestVersionNumber:
+          self.recordingLatestVersionNumber += 1
+          return
+    foundLatestVersionNumberTag = False
     for name, value in attributes:
       if name == 'id' and value == 'MassiveLauncherLatestVersionNumber':
+        foundLatestVersionNumberTag = True
         break
     else:
       return
-    self.recording = 1
+    if foundLatestVersionNumberTag:
+        self.recordingLatestVersionNumber = 1
 
   def handle_endtag(self, tag):
-    if tag == 'span' and self.recording:
-      self.recording -= 1
+    if tag == 'span' and self.recordingLatestVersionNumber:
+      self.recordingLatestVersionNumber -= 1
 
   def handle_data(self, data):
-    if self.recording:
-      self.data.append(data)
+    if self.recordingLatestVersionNumber:
+      #self.data.append(data)
+      self.latestVersionNumber = data.strip()
+
+  def handle_comment(self,data):
+      self.htmlComments += data.strip()
 
 class LauncherMainFrame(wx.Frame):
 
@@ -677,9 +688,13 @@ class LauncherMainFrame(wx.Frame):
             sys.exit(1)
 
 
-        latestVersion = myHtmlParser.data[0].strip()
+        latestVersionNumber = myHtmlParser.latestVersionNumber
+        htmlComments = myHtmlParser.htmlComments
+        htmlCommentsSplit1 = htmlComments.split("<pre id=\"CHANGES\">")
+        htmlCommentsSplit2 = htmlCommentsSplit1[1].split("</pre>")
+        latestVersionChanges = htmlCommentsSplit2[0].strip()
 
-        if latestVersion!=launcher_version_number.version_number:
+        if latestVersionNumber!=launcher_version_number.version_number:
             newVersionAlertDialog = wx.Dialog(launcherMainFrame, title="MASSIVE/CVL Launcher", name="MASSIVE/CVL Launcher",pos=(200,150),size=(680,290))
 
             if sys.platform.startswith("win"):
@@ -699,25 +714,25 @@ class LauncherMainFrame(wx.Frame):
                 (0, 50),
                 (massiveIconAsBitmap.GetWidth(), massiveIconAsBitmap.GetHeight())) 
 
-            newVersionAlertTextPanel = wx.Panel(newVersionAlertDialog)
+            newVersionAlertPanel = wx.Panel(newVersionAlertDialog)
 
-            newVersionAlertTextPanelSizer = wx.FlexGridSizer(rows=4, cols=1, vgap=5, hgap=5)
-            newVersionAlertTextPanel.SetSizer(newVersionAlertTextPanelSizer)
+            newVersionAlertPanelSizer = wx.FlexGridSizer(rows=8, cols=1, vgap=5, hgap=5)
+            newVersionAlertPanel.SetSizer(newVersionAlertPanelSizer)
 
-            newVersionAlertTitleLabel = wx.StaticText(newVersionAlertTextPanel,
+            newVersionAlertTitleLabel = wx.StaticText(newVersionAlertPanel,
                 label = "MASSIVE/CVL Launcher")
             font = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
             font.SetPointSize(14)
             font.SetWeight(wx.BOLD)
             newVersionAlertTitleLabel.SetFont(font)
-            newVersionAlertTextPanelSizer.Add(wx.StaticText(newVersionAlertTextPanel))
-            newVersionAlertTextPanelSizer.Add(newVersionAlertTitleLabel, flag=wx.EXPAND)
-            newVersionAlertTextPanelSizer.Add(wx.StaticText(newVersionAlertTextPanel))
+            newVersionAlertPanelSizer.Add(wx.StaticText(newVersionAlertPanel))
+            newVersionAlertPanelSizer.Add(newVersionAlertTitleLabel, flag=wx.EXPAND)
+            newVersionAlertPanelSizer.Add(wx.StaticText(newVersionAlertPanel))
 
-            newVersionAlertTextLabel1 = wx.StaticText(newVersionAlertTextPanel, 
+            newVersionAlertTextLabel1 = wx.StaticText(newVersionAlertPanel, 
                 label = 
                 "You are running version " + launcher_version_number.version_number + "\n\n" +
-                "The latest version is " + myHtmlParser.data[0] + "\n\n" +
+                "The latest version is " + latestVersionNumber + "\n\n" +
                 "Please download a new version from:\n\n")
             font = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
             if sys.platform.startswith("darwin"):
@@ -725,9 +740,9 @@ class LauncherMainFrame(wx.Frame):
             else:
                 font.SetPointSize(9)
             newVersionAlertTextLabel1.SetFont(font)
-            newVersionAlertTextPanelSizer.Add(newVersionAlertTextLabel1, flag=wx.EXPAND)
+            newVersionAlertPanelSizer.Add(newVersionAlertTextLabel1, flag=wx.EXPAND)
 
-            newVersionAlertHyperlink = wx.HyperlinkCtrl(newVersionAlertTextPanel, 
+            newVersionAlertHyperlink = wx.HyperlinkCtrl(newVersionAlertPanel, 
                 id = wx.ID_ANY,
                 label = launcherURL,
                 url = launcherURL)
@@ -737,34 +752,70 @@ class LauncherMainFrame(wx.Frame):
             else:
                 font.SetPointSize(8)
             newVersionAlertHyperlink.SetFont(font)
-            newVersionAlertTextPanelSizer.Add(newVersionAlertHyperlink, flag=wx.EXPAND)
-            newVersionAlertTextPanelSizer.Add(wx.StaticText(newVersionAlertTextPanel))
+            newVersionAlertPanelSizer.Add(newVersionAlertHyperlink, border=10, flag=wx.LEFT|wx.BORDER)
+            newVersionAlertPanelSizer.Add(wx.StaticText(newVersionAlertPanel))
 
-            newVersionAlertTextLabel2 = wx.StaticText(newVersionAlertTextPanel, 
+            self.latestVersionChangesTextCtrl = wx.TextCtrl(newVersionAlertPanel, 
+                size=(600, 200), style=wx.TE_MULTILINE|wx.TE_READONLY)
+            newVersionAlertPanelSizer.Add(self.latestVersionChangesTextCtrl, flag=wx.EXPAND)
+            if sys.platform.startswith("darwin"):
+                font = wx.Font(11, wx.MODERN, wx.NORMAL, wx.NORMAL, False, u'Courier New')
+            else:
+                font = wx.Font(9, wx.MODERN, wx.NORMAL, wx.NORMAL, False, u'Courier New')
+            self.latestVersionChangesTextCtrl.SetFont(font)
+            self.latestVersionChangesTextCtrl.AppendText(latestVersionChanges)
+            self.latestVersionChangesTextCtrl.SetInsertionPoint(0)
+
+            newVersionAlertPanelSizer.Add(wx.StaticText(newVersionAlertPanel, wx.ID_ANY, ""))
+            newVersionAlertQueriesContactLabel = wx.StaticText(newVersionAlertPanel, 
                 label = 
-                "For queries, please contact:\n\nhelp@massive.org.au\njames.wettenhall@monash.edu\n")
+                "For queries, please contact:")
             font = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
             if sys.platform.startswith("darwin"):
                 font.SetPointSize(11)
             else:
                 font.SetPointSize(9)
-            newVersionAlertTextLabel2.SetFont(font)
-            newVersionAlertTextPanelSizer.Add(newVersionAlertTextLabel2, flag=wx.EXPAND)
+            newVersionAlertQueriesContactLabel.SetFont(font)
+            newVersionAlertPanelSizer.Add(newVersionAlertQueriesContactLabel, border=10, flag=wx.EXPAND|wx.BORDER)
+
+            contactEmailHyperlink = wx.HyperlinkCtrl(newVersionAlertPanel, 
+                id = wx.ID_ANY,
+                label = "help@massive.org.au",
+                url = "mailto:help@massive.org.au")
+            font = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
+            if sys.platform.startswith("darwin"):
+                font.SetPointSize(11)
+            else:
+                font.SetPointSize(8)
+            contactEmailHyperlink.SetFont(font)
+            newVersionAlertPanelSizer.Add(contactEmailHyperlink, border=20, flag=wx.LEFT|wx.BORDER)
+
+            contactEmail2Hyperlink = wx.HyperlinkCtrl(newVersionAlertPanel, 
+                id = wx.ID_ANY,
+                label = "James.Wettenhall@monash.edu",
+                url = "mailto:James.Wettenhall@monash.edu")
+            font = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
+            if sys.platform.startswith("darwin"):
+                font.SetPointSize(11)
+            else:
+                font.SetPointSize(8)
+            contactEmail2Hyperlink.SetFont(font)
+            newVersionAlertPanelSizer.Add(contactEmail2Hyperlink, border=20, flag=wx.LEFT|wx.BORDER)
 
             def onOK(event):
                 sys.exit(1)
 
-            okButton = wx.Button(newVersionAlertTextPanel, 1, ' OK ')
+            okButton = wx.Button(newVersionAlertPanel, 1, ' OK ')
             okButton.SetDefault()
-            newVersionAlertTextPanelSizer.Add(okButton, flag=wx.ALIGN_RIGHT)
-            newVersionAlertTextPanelSizer.Add(wx.StaticText(newVersionAlertTextPanel))
-            newVersionAlertTextPanelSizer.Fit(newVersionAlertTextPanel)
+            newVersionAlertPanelSizer.Add(okButton, flag=wx.ALIGN_RIGHT)
+            newVersionAlertPanelSizer.Add(wx.StaticText(newVersionAlertPanel))
+            newVersionAlertPanelSizer.Fit(newVersionAlertPanel)
 
             newVersionAlertDialog.Bind(wx.EVT_BUTTON, onOK, id=1)
 
             newVersionAlertDialogSizer = wx.FlexGridSizer(rows=1, cols=3, vgap=5, hgap=5)
             newVersionAlertDialogSizer.Add(massiveIconPanel, flag=wx.EXPAND)
-            newVersionAlertDialogSizer.Add(newVersionAlertTextPanel, flag=wx.EXPAND)
+            newVersionAlertDialogSizer.Add(newVersionAlertPanel, flag=wx.EXPAND)
             newVersionAlertDialogSizer.Add(wx.StaticText(newVersionAlertDialog,label="       "))
             newVersionAlertDialog.SetSizer(newVersionAlertDialogSizer)
             newVersionAlertDialogSizer.Fit(newVersionAlertDialog)
