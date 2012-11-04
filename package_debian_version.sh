aaa5fffc30fad6f83b515ef2ed5e8b5aba02facf
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2012-11-05 09:54:55 +1000

    Fixing the Debian build script: puts the package into /opt/MassiveLauncher with no version number or architecture.

--- package_debian_version.sh
+++ package_debian_version.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-sudo apt-get --assume-yes install 	python-wxgtk2.8 python-wxgtk2.8-dbg \
-					python-pycryptopp python-pycryptopp-dbg python-libssh2 \
-					python-setuptools python-dev
-
-sudo easy_install pip 
-sudo pip install ssh
-sudo pip install appdirs
-sudo pip install psutil
-
 VERSION=`grep '^version_number' launcher_version_number.py | cut -f 2 -d '"'`
 ARCHITECTURE=`uname -m | sed s/x86_64/amd64/g | sed s/i686/i386/g`
 
@@ -19,7 +10,7 @@ TMP="tmp_debian_build"
 rm -fr $TMP
 rm -f *.deb
 
-TARGET=$TMP/opt/MassiveLauncher-${VERSION}_${ARCHITECTURE}
+TARGET=$TMP/opt/MassiveLauncher
 mkdir -p $TARGET
 
 cp -r dist/MassiveLauncher-${VERSION}_${ARCHITECTURE}/* $TARGET/
@@ -28,10 +19,6 @@ cp release/control $TMP/DEBIAN
 
 sed -i "s/VERSION/${VERSION}/g" $TMP/DEBIAN/control
 sed -i "s/ARCHITECTURE/${ARCHITECTURE}/g" $TMP/DEBIAN/control
-
-sed -i "s@/opt/MassiveLauncher@/opt/MassiveLauncher-${VERSION}_${ARCHITECTURE}@g" \
-    ${TARGET}/MASSIVE\ Launcher.desktop \
-    ${TARGET}/massiveLauncher.sh
 
 DEB=massive-launcher_${VERSION}_${ARCHITECTURE}.deb
 dpkg -b $TMP $DEB
