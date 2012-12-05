8a2cc8954745ff43ece1f0046d58537b5b1b55e8
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2012-12-05 11:41:25 +1000

    No git package in Centos 5, so changing the build scripts to wget the tarball of the latest master branch and build from that instead.

--- system_build_scripts/build_launcher_centos_32bit.sh
+++ system_build_scripts/build_launcher_centos_32bit.sh
@@ -4,8 +4,11 @@ WDIR=${HOME}/${1}
 
 mkdir $WDIR
 cd $WDIR
-git clone git@github.com:CVL-dev/cvl-fabric-launcher.git
-cd cvl-fabric-launcher
+rm -f master.tar.gz
+wget https://github.com/CVL-dev/cvl-fabric-launcher/archive/master.tar.gz
+tar zxf master.tar.gz
+cd cvl-fabric-launcher-master
+
 module load python wxpython
 ./package_centos_version.sh
 
