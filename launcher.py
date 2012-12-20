b5485634b83a41256d1c38b934a04ca408d63559
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2012-12-20 11:14:11 +1000

    Removing debug rubbish.

--- launcher.py
+++ launcher.py
@@ -2134,14 +2134,12 @@ class LauncherMainFrame(wx.Frame):
                                     logger.debug('launcherMainFrame.cvlVncDisplayNumberAutomatic == True')
 
                                     def askCvlUserWhetherTheyWantToKeepOrDiscardTheirVncSession(sshClient2):
-                                        print "hey!"
                                         import questionDialog
                                         result = questionDialog.questionDialog("Do you want to keep your VNC session (Display #" + str(self.cvlVncDisplayNumber) + ") running for future use?",
                                             #buttons=["Discard VNC Session", wx.ID_CANCEL, "Save VNC Session"])
                                             buttons=["Discard VNC Session", "Save VNC Session"],
                                             caption="MASSIVE/CVL Launcher")
                                         if result == "Discard VNC Session":
-                                            print 'Discarding!!!!'
                                             cvlVncSessionStopCommand = "vncsession stop " + str(self.cvlVncDisplayNumber)
                                             logger.debug('cvlVncSessionStopCommand: ' + cvlVncSessionStopCommand)
 
