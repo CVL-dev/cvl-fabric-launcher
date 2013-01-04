6edc19a12708a87ce7dd22bd7169c2a807c561aa
James Wettenhall <James.Wettenhall@monash.edu>
2013-01-04 14:43:14 +1100

    Moved pyinstaller-2.0 directory from Z:\Desktop\ to C:\

--- package_windows_version.py
+++ package_windows_version.py
@@ -20,7 +20,7 @@ os.system('del /Q dist\\*.*')
 #os.system('copy /Y C:\\WINDOWS\\WinSxS\\Manifests\\' + vc90_dirs[0] + '.manifest')
 
 #os.system('\\Python27\\python.exe create_windows_bundle.py py2exe')
-os.system('C:\\Python27\\python.exe  Z:\\Desktop\\pyinstaller-2.0\\pyinstaller.py launcher.py')
+os.system('C:\\Python27\\python.exe  C:\\pyinstaller-2.0\\pyinstaller.py launcher.py')
 
 #os.system('copy /Y MASSIVE.ico dist\\')
 os.system('copy /Y MASSIVE.ico dist\\launcher\\')
