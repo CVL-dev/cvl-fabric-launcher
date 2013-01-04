aa93e409409b5802ce4f64d9fdfd85d75ab2ac3c
James Wettenhall <James.Wettenhall@monash.edu>
2013-01-04 14:39:46 +1100

    Now uses PyInstaller instead of py2exe. PyInstaller can take care of the Manifest stuff itself.

--- package_windows_version.py
+++ package_windows_version.py
@@ -4,34 +4,41 @@ import glob
 
 os.system('del /Q dist\\*.*')
 
-original_dir = os.getcwd()
+# PyInstaller should take care of the Manifests stuff.
 
-os.chdir('C:\\WINDOWS\\WinSxS')
+#original_dir = os.getcwd()
 
-vc90_dirs = glob.glob('x86_Microsoft.VC90.CRT_*21022*')
+#os.chdir('C:\\WINDOWS\\WinSxS')
 
-assert len(vc90_dirs) == 1
+#vc90_dirs = glob.glob('x86_Microsoft.VC90.CRT_*21022*')
 
-os.chdir(original_dir)
+#assert len(vc90_dirs) == 1
 
-os.system('copy /Y C:\\WINDOWS\\WinSxS\\' + vc90_dirs[0] + '\\msvcp90.dll')
-os.system('copy /Y C:\\WINDOWS\\WinSxS\\Manifests\\' + vc90_dirs[0] + '.manifest')
+#os.chdir(original_dir)
 
-os.system('\\Python27\\python.exe create_windows_bundle.py py2exe')
+#os.system('copy /Y C:\\WINDOWS\\WinSxS\\' + vc90_dirs[0] + '\\msvcp90.dll')
+#os.system('copy /Y C:\\WINDOWS\\WinSxS\\Manifests\\' + vc90_dirs[0] + '.manifest')
 
-os.system('copy /Y MASSIVE.ico dist\\')
-os.system('copy /Y C:\\Python27\\Lib\\site-packages\\wx-2.8-msw-unicode\\wx\\gdiplus.dll dist\\')
-os.system('copy /Y sshwindows\\*.* dist\\')
-os.system('copy /Y GPL.txt dist\\')
+#os.system('\\Python27\\python.exe create_windows_bundle.py py2exe')
+os.system('C:\\Python27\\python.exe  Z:\\Desktop\\pyinstaller-2.0\\pyinstaller.py launcher.py')
+
+#os.system('copy /Y MASSIVE.ico dist\\')
+os.system('copy /Y MASSIVE.ico dist\\launcher\\')
+#os.system('copy /Y C:\\Python27\\Lib\\site-packages\\wx-2.8-msw-unicode\\wx\\gdiplus.dll dist\\')
+os.system('copy /Y C:\\Python27\\Lib\\site-packages\\wx-2.8-msw-unicode\\wx\\gdiplus.dll dist\\launcher\\')
+#os.system('copy /Y sshwindows\\*.* dist\\')
+os.system('copy /Y sshwindows\\*.* dist\\launcher\\')
+#os.system('copy /Y GPL.txt dist\\')
+os.system('copy /Y GPL.txt dist\\launcher\\')
 
 # REM Is the filename of the manifest file safe to assume?
 
-os.system('copy /Y *.manifest dist\\Microsoft.VC90.CRT')
-os.chdir('dist\\Microsoft.VC90.CRT')
-os.system('rename ' + glob.glob('*manifest*')[0] + ' Microsoft.VC90.CRT.manifest')
-os.chdir('..\\..')
+#os.system('copy /Y *.manifest dist\\Microsoft.VC90.CRT')
+#os.chdir('dist\\Microsoft.VC90.CRT')
+#os.system('rename ' + glob.glob('*manifest*')[0] + ' Microsoft.VC90.CRT.manifest')
+#os.chdir('..\\..')
 
 # Only one of these will work...
-os.system(r""""C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc launcherWindowsSetupWizardScript.iss""")
-os.system(r""""C:\Program Files\Inno Setup 5\Compil32.exe" /cc launcherWindowsSetupWizardScript.iss""")
+os.system(r""""C:\Program Files (x86)\Inno Setup 5\Compil32.exe" /cc .\\launcherWindowsSetupWizardScript.iss""")
+os.system(r""""C:\Program Files\Inno Setup 5\Compil32.exe" /cc .\\launcherWindowsSetupWizardScript.iss""")
 
