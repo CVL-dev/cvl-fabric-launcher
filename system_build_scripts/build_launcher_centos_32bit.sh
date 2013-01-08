7dab6874cfe3dca298a7e6143e9a5eb805aae70e
James Wettenhall <James.Wettenhall@monash.edu>
2013-01-09 09:21:26 +1100

    - Added a check for number of command-line arguments. - Added a check to see if the build directory already exists. - Build dir no longer needs to be a subdirectory of $HOME

--- system_build_scripts/build_launcher_centos_32bit.sh
+++ system_build_scripts/build_launcher_centos_32bit.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-WDIR=${HOME}/${1}
+if [ $# -lt 1 ]; then
+    echo "Usage: build_launcher_centos_32bit.sh <build_directory>"
+    exit 1
+fi
+
+#WDIR=${HOME}/${1}
+WDIR=${1}
+
+if [ -d $WDIR ]; then
+    echo "Error: Please remove the existing \"$WDIR\" directory. It will be created automatically."
+    exit 1
+fi
 
 mkdir $WDIR
 cd $WDIR
