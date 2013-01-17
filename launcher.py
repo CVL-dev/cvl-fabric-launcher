bfbedc04924ada69569a303f3779fc1db7c31299
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-01-17 11:39:24 +1000

    Reverting previous change.

--- launcher.py
+++ launcher.py
@@ -1230,7 +1230,7 @@ class LauncherMainFrame(wx.Frame):
                 self.updatingProgressDialog = True
                 if launcherMainFrame.progressDialog!=None:
                     launcherMainFrame.progressDialog.Update(value, message)
-                    self.shouldAbort = launcherMainFrame.progressDialog.shouldAbort
+                    self.shouldAbort = launcherMainFrame.progressDialog.shouldAbort()
                 self.updatingProgressDialog = False
                 
             def updateTidyingUpProgressDialog(self, value, message):
@@ -2054,7 +2054,7 @@ class LauncherMainFrame(wx.Frame):
                         while True:
                             tCheck = 0
 
-                            self.shouldAbort = launcherMainFrame.progressDialog.shouldAbort
+                            self.shouldAbort = launcherMainFrame.progressDialog.shouldAbort()
                             if self.shouldAbort:
                                 deleteMassiveJobIfNecessary(write_debug_log=True,update_status_bar=True,update_main_progress_bar=True,update_tidying_up_progress_bar=False,ignore_errors=False)
                                 wx.CallAfter(launcherMainFrame.progressDialog.Show, False)
