37b008f7a870cc47a1f1e26c00ffe90b447d15b0
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-01-24 10:35:22 +1000

    SSL socket hello-world example with notes on making a self-signed certificate. In production code we will need to use a proper certificate.

---
+++ tmp_vm_pool/client.py
@@ -0,0 +1,30 @@
+import socket, ssl, pprint
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Require a certificate from the server. We used a self-signed certificate
+# so here ca_certs must be the server certificate itself.
+ssl_sock = ssl.wrap_socket(s,
+                           ca_certs="server.crt",
+                           cert_reqs=ssl.CERT_REQUIRED)
+
+ssl_sock.connect(('localhost', 10023))
+
+print repr(ssl_sock.getpeername())
+print ssl_sock.cipher()
+print pprint.pformat(ssl_sock.getpeercert())
+
+ssl_sock.write("boo!")
+
+if False: # from the Python 2.7.3 docs
+    # Set a simple HTTP request -- use httplib in actual code.
+    ssl_sock.write("""GET / HTTP/1.0\r
+    Host: www.verisign.com\r\n\r\n""")
+
+    # Read a chunk of data.  Will not necessarily
+    # read all the data returned by the server.
+    data = ssl_sock.read()
+
+    # note that closing the SSLSocket will also close the underlying socket
+    ssl_sock.close()
+
