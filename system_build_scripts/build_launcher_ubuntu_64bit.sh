dcaa0a833a744727abb67978be867b0ef8d80ad1
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-05-10 01:56:28 +0000

    Build scripts for Ubuntu.

---
+++ system_build_scripts/build_launcher_ubuntu_64bit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "Usage: build_launcher_ubuntu_64bit.sh <build_directory>"
+    exit 1
+fi
+
+#WDIR=${HOME}/${1}
+WDIR=${1}
+
+if [ -d $WDIR ]; then
+    echo "Error: Please remove the existing \"$WDIR\" directory. It will be created automatically."
+    exit 1
+fi
+
+mkdir $WDIR
+cd $WDIR
+git clone git@github.com:CVL-dev/cvl-fabric-launcher.git
+if [ $? -ne 0 ]; then
+    echo ""
+    echo "*** Attempting to use git with SSH keys failed. ***"
+    echo "*** Cloning the Launcher's repository as read-only instead. ***"
+    echo ""
+    git clone git://github.com/CVL-dev/cvl-fabric-launcher
+fi
+cd cvl-fabric-launcher
+./package_ubuntu_version.sh
