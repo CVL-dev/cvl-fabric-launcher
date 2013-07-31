d8b92bb1339572a7233409a9f10509048c73d234
James Wettenhall <James.Wettenhall@monash.edu>
2013-07-31 12:12:07 +1000

    Ensuring that temporary Python module name only contains legal characters.

--- import_remote_test_module.py
+++ import_remote_test_module.py
@@ -1,10 +1,20 @@
+#!/usr/bin/python
 import tempfile
 import requests
 import traceback
+
+# Hack from http://stackoverflow.com/questions/12522844/change-character-set-for-tempfile-namedtemporaryfile
+# to ensure that the temporary Python module filename only contains characters which are legal for
+# Python module names.
+class MyRandomSequence(tempfile._RandomNameSequence):
+    import string
+    characters = string.letters
+tempfile._name_sequence = MyRandomSequence()
+
 try:
     # Download test.py
 
-    testModuleFile = tempfile.NamedTemporaryFile(mode='w+b', prefix='test', suffix='.py', delete=False)
+    testModuleFile = tempfile.NamedTemporaryFile(mode='w+b', prefix='test_', suffix='.py', delete=False)
     testModuleFilePath = testModuleFile.name
     print "testModuleFilePath = " + testModuleFilePath
     r = requests.get("https://raw.github.com/CVL-dev/cvl-fabric-launcher/JamesJuly30/test.py", verify=False)
@@ -20,8 +30,8 @@ try:
     print "testModuleFileName = " + testModuleFileName
     testModuleName = testModuleFileName.split(".")[0]
     print "testModuleName = " + testModuleName
-    importCommand = "import %s" % (testModuleName)
-    print "importCommand:  %s" % (importCommand)
+    importCommand = "import " + testModuleName
+    print "importCommand: " + importCommand
     exec importCommand
 except:
     print "Failed to download remote test.py module."
