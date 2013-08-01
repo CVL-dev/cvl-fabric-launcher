588931e34f723c8bd3d672c39d0e72eb87355965
James Wettenhall <James.Wettenhall@monash.edu>
2013-08-01 22:28:43 +1000

    Added @ECHO OFF to batch file.

--- kill-charade-processes.bat
+++ kill-charade-processes.bat
@@ -1,3 +1,4 @@
+@ECHO OFF
 REM FOR /F "usebackq tokens=2" %i IN (`tasklist ^| find str /r /b "charade.exe"`) DO taskkill /F /pid %i
 tasklist /FI "IMAGENAME eq charade.exe" 2>NUL | find /I /N "charade.exe">NUL
 if "%ERRORLEVEL%"=="0" taskkill /f /im charade.exe
