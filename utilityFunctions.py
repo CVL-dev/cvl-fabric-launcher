834f3b12c807a053da2cd44294c0c5c8f9fdb390
James Wettenhall <James.Wettenhall@monash.edu>
2013-08-13 22:04:30 +1000

    Fixed spelling/typo: "latter" => "later".

--- utilityFunctions.py
+++ utilityFunctions.py
@@ -46,10 +46,10 @@ This option can be changed from the Identity menu.
         self.temporaryKeyNo="Use my SSH Key"
         self.qdelQueuedJob="""
 It looks like you've been waiting for a job to start.
-Do you want me to delete the job or leave it in the queue so you can reconnect latter?"
+Do you want me to delete the job or leave it in the queue so you can reconnect later?"
 """
         self.qdelQueuedJobQdel="Delete the job"
-        self.qdelQueuedJobNOOP="Leave it in the queue (I'll reconnect latter)"
+        self.qdelQueuedJobNOOP="Leave it in the queue (I'll reconnect later)"
 
 class sshKeyDistDisplayStringsCVL(sshKeyDistDisplayStrings):
     def __init__(self):
