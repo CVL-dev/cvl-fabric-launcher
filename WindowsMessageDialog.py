4bb48d3fc34b84319567692c76f115bea81de2e0
James Wettenhall <james.wettenhall@monash.edu>
2013-09-04 22:47:36 +1000

    Trying to make WindowsMessageDialog look more native on Windows.

--- WindowsMessageDialog.py
+++ WindowsMessageDialog.py
@@ -13,25 +13,34 @@ class LauncherMessageDialog(wx.Dialog):
 
         self.dialogPanel = wx.Panel(self, wx.ID_ANY)
 
-        iconAsBitmap = IconPys.MASSIVElogoTransparent64x64.getMASSIVElogoTransparent64x64Bitmap()
-        self.iconBitmap = wx.StaticBitmap(self.dialogPanel, wx.ID_ANY, iconAsBitmap, pos=(25,15), size=(64,64))
+        import sys
+        if sys.platform.startswith("win"):
+            _icon = wx.Icon('MASSIVE.ico', wx.BITMAP_TYPE_ICO)
+            self.SetIcon(_icon)
+        elif sys.platform.startswith("linux"):
+            import MASSIVE_icon
+            self.SetIcon(MASSIVE_icon.getMASSIVElogoTransparent128x128Icon())
 
-        self.titleLabel = wx.StaticText(self.dialogPanel, wx.ID_ANY, title, pos=(105,15))
-        titleFont = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
-        titleFont.SetWeight(wx.BOLD) 
-        titleFont.SetPointSize(13)
-        self.titleLabel.SetFont(titleFont) 
+        #self.iconBitmap = wx.StaticBitmap(self.dialogPanel,bitmap=wx.ArtProvider.GetBitmap(wx.ART_INFORMATION), pos=(25,15), size=(32,32))
+        iconAsBitmap = IconPys.MASSIVElogoTransparent64x64.getMASSIVElogoTransparent64x64Bitmap()
+        self.iconBitmap = wx.StaticBitmap(self.dialogPanel,bitmap=iconAsBitmap, pos=(25,15), size=(64,64))
+
+        self.SetTitle(title)
 
         smallFont = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
-        smallFont.SetPointSize(11)
+        #smallFont.SetPointSize(11)
 
-        messageWidth = 330
-        self.messageLabel = wx.StaticText(self.dialogPanel, wx.ID_ANY, message, pos=(105,39), size=(messageWidth,-1))
+        self.messageLabel = wx.StaticText(self.dialogPanel, wx.ID_ANY, message)
+        messageWidth = self.messageLabel.GetSize().width
+        self.messageLabel.Destroy()
+        messageWidth = min(messageWidth,450)
+        self.messageLabel = wx.StaticText(self.dialogPanel, wx.ID_ANY, message, pos=(105,15), size=(messageWidth,-1))
         self.messageLabel.SetForegroundColour((0,0,0))
         self.messageLabel.SetFont(smallFont)
         self.messageLabel.Wrap(messageWidth)
+        self.dialogPanel.Layout()
 
-        dialogPanelWidth = 475
+        dialogPanelWidth = messageWidth + 125
         dialogPanelHeight = max(self.messageLabel.GetPosition().y + self.messageLabel.GetSize().height + 70, 140)
 
         self.SetClientSize(wx.Size(dialogPanelWidth,dialogPanelHeight))
@@ -46,27 +55,19 @@ class LauncherMessageDialog(wx.Dialog):
         self.Bind(wx.EVT_CLOSE, self.onClose)
         self.Bind(wx.EVT_BUTTON, self.onClose, id=self.okButton.GetId())
 
-        self.contactQueriesContactLabel = wx.StaticText(self.dialogPanel, label = "For queries, please contact:")
-        self.contactQueriesContactLabel.SetFont(smallFont)
-        self.contactQueriesContactLabel.SetForegroundColour(wx.Colour(0,0,0))
-        self.contactQueriesContactLabel.SetPosition(wx.Point(25,okButtonPosition.y))
+        self.queriesContactLabel = wx.StaticText(self.dialogPanel, label = "For queries, please contact:")
+        self.queriesContactLabel.SetFont(smallFont)
+        self.queriesContactLabel.SetForegroundColour(wx.Colour(0,0,0))
+        self.queriesContactLabel.SetPosition(wx.Point(25,okButtonPosition.y))
 
         self.contactEmailHyperlink = wx.HyperlinkCtrl(self.dialogPanel, id = wx.ID_ANY, label = "help@massive.org.au", url = "mailto:help@massive.org.au")
         self.contactEmailHyperlink.SetFont(smallFont) # Or maybe even smaller font?
-        #hyperlinkPosition = wx.Point(self.contactQueriesContactLabel.GetPosition().x+self.contactQueriesContactLabel.GetSize().width+10,okButtonPosition.y)
-        hyperlinkPosition = wx.Point(self.contactQueriesContactLabel.GetPosition().x+self.contactQueriesContactLabel.GetSize().width,okButtonPosition.y)
+        #hyperlinkPosition = wx.Point(self.queriesContactLabel.GetPosition().x+self.queriesContactLabel.GetSize().width+10,okButtonPosition.y)
+        hyperlinkPosition = wx.Point(self.queriesContactLabel.GetPosition().x+self.queriesContactLabel.GetSize().width+10,okButtonPosition.y)
         self.contactEmailHyperlink.SetPosition(hyperlinkPosition)
+        self.contactEmailHyperlink.SetSize(self.contactEmailHyperlink.GetBestSize())
 
     def onClose(self, event):
         self.Show(False) 
         self.Destroy()
 
-class MyApp(wx.App):
-    def OnInit(self):
-        message = "You have requested 2880 CPU hours, but you only have 455.0 CPU hours remaining in your quota for project \"Desc002\"."
-        dialog = LauncherMessageDialog(parent=None, message=message, title="MASSIVE/CVL Launcher")
-        dialog.ShowModal()
-        return True
-
-#app = MyApp(False)
-#app.MainLoop()
