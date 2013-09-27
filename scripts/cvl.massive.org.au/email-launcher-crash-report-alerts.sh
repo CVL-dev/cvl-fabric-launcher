da1e964873ee476ce9ae1c5adc663b250e4f8eff
James Wettenhall <James.Wettenhall@monash.edu>
2013-09-27 10:13:14 +1000

    Added new cases for emailing the Launcher's debug report to cvl-help@monash.edu instead of help@massive.org.au.  We can probably drop some of the other email addresses from the recipients list now, given that I think all of us get email alerts for emails received by cvl-help@monash.edu.

--- scripts/cvl.massive.org.au/email-launcher-crash-report-alerts.sh
+++ scripts/cvl.massive.org.au/email-launcher-crash-report-alerts.sh
@@ -21,6 +21,8 @@ do
     originalRecipients=$recipients
     grep -q "Contact me? Yes" $filename && grep -q "^Config: login.cvl.massive.org.au" $filename && recipients="cvl-help@monash.edu,${recipients}"
     grep -q "Contact me? Yes" $filename && grep -q "^Config: Huygens" $filename && recipients="cvl-help@monash.edu,${recipients}"
+    grep -q "Contact me? Yes" $filename && grep -q "^Config: 115.146" $filename && recipients="cvl-help@monash.edu,${recipients}"
+    grep -q "Contact me? Yes" $filename && grep -q "^Config: 118.138" $filename && recipients="cvl-help@monash.edu,${recipients}"
     if [ $recipients == $originalRecipients ]
     then
         grep -q "Contact me? Yes" $filename && recipients="help@massive.org.au,${recipients}"
