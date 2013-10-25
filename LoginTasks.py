2a7614034997809646aa3056048ca35435ebf952
Chris Hines <chris.hines@monash.edu>
2013-10-25 15:14:50 +1100

    add a timeout to run_command (it seems sometimes simple things like qstat -u {username} can hang indefinitly

--- LoginTasks.py
+++ LoginTasks.py
@@ -965,6 +965,10 @@ class LoginProcess():
                 if (event.loginprocess.siteConfig.showStart.cmd!=None):
                     logger.debug('loginProcessEvent: event.loginprocess.showStartCmd is not None, so posting EVT_LOGINPROCESS_SHOW_ESTIMATED_START')
                     nextevent=LoginProcess.loginProcessEvent(LoginProcess.EVT_LOGINPROCESS_SHOW_ESTIMATED_START,event.loginprocess)
+                    # Cleanup any old threads before we launch a new one, just incase we are hitting a max threads limit
+                    for t in event.loginprocess.threads:
+                        if not t.is_alive():
+                            event.loginprocess.threads.remove(t)
                     t = LoginProcess.runServerCommandThread(event.loginprocess,event.loginprocess.siteConfig.showStart,nextevent,"Error estimating the start time")
                     t.setDaemon(False)
                     t.start()
